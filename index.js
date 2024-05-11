e84686004df792101fc0691a0a413a6a1760d603
Pat <patsandesu@gmail.com>
2024-05-12 00:33:15 +0800

    CodeHelixCorp

---
+++ index.js
@@ -0,0 +1,56 @@
+ // Smooth scrolling for anchor links
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Philippines</title>
+    <link rel="stylesheet" href="Index.css">
+</head>
+<body>
+        <!-- header -->
+    <header>
+        <div class="logo">CodeHelixCorp</div>
+        <ul class="menu">
+        <li><a href="#list">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#att">More</a></li>
+            <li><a href="attraction.php">Attractions</a></li> 
+        </ul>
+    </header>
+
+    <!-- slider -->
+
+    <div class="slider">
+        <!-- list Items -->
+        <div class="list">
+            <div class="item active">
+                <img src="Img/Black Elegant and Modern Startup Pitch Deck Presentation.png">
+                <div class="content">
+                    <p>Welcome at</p>
+                    <h2>CodeHelix Corp</h2>
+                    <p>
+                    In 2024, CodeHelix was founded by a group of students driven by their enthusiasm and determination to make a mark in 
+                    the tech world. The name CodeHelix is derived from the word “Helix” which typically refers to a three-dimensional 
+                    curve or spiral shape,  However in a broader sense “Helix” can symbolize growth, progress, and continuous development, 
+                    as it suggest a spiral motion indicating advancement and evolution. 
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/2.png">
+                <div class="content">
+                    <p>Alaminos, Pangasinan</p>
+                    <h2>Hundred Island</h2>
+                    <p>
+                    CodeHelix offers a wide range of product designed to meet our customers needs.
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/3.png">
+                <div class="content">
+                    <p>El Nido, Palawan</p>
+                    <h2>Las Cabanas</h2>
+                    <p>                
+                    Las Cabanas Beach is a renowned coastal gem situated near El Nido in Palawan, Philippines. With its powdery white
+                     sand, crystal-clear turquoise waters, and dramatic limestone cliffs, Las Cabanas Beach offers a breathtaking 
+                     tropical escape.
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/4.png">
+                <div class="content">
+                    <p>Nasugbu, Batangas</p>
+                    <h2>Little Boracay</h2>
+                    <p>
+                    "Little Boracay" is a charming beach destination located in Nasugbu, Batangas, Philippines. Known for its pristine
+                     white sand shores and clear turquoise waters, Little Boracay offers visitors a serene and picturesque getaway 
+                     reminiscent of the famous Boracay Island. 
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/5.png">
+                <div class="content">
+                    <p>Basco, Batanes</p>
+                    <h2>Basco Lighthouse</h2>
+                    <p>
+                    The Basco Lighthouse is an iconic landmark located in Basco, the capital of Batanes province in the Philippines. Perched atop Naidi Hill,
+                     this picturesque lighthouse offers panoramic views of the surrounding landscapes, including the rugged coastlines 
+                     and the vast expanse of the West Philippine Sea.
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/6.png">
+                <div class="content">
+                    <p>Vigan</p>
+                    <h2>Calle Crisologo</h2>
+                    <p>          
+                    Calle Crisologo is a historic cobblestone street located in Vigan City, Philippines. It is renowned for its well-preserved Spanish 
+                    colonial architecture, which dates back to the 16th century.  
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/7.png">
+                <div class="content">
+                    <p>Intramuros, Manila</p>
+                    <h2>Fort Santiago</h2>
+                    <p>      
+                        Fort Santiago is a historic citadel located in Intramuros, Manila, Philippines. Built by Spanish conquistador Miguel López de 
+                        Legazpi in the late 16th century, it served as a defensive fortress to protect Manila from foreign invasions.
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/8.png">
+                <div class="content">
+                    <p>Sabtang, Batanes</p>
+                    <h2>Chamantad Tinan </h2>
+                    <p>
+                    Chamantad Tinan Cove is a stunning natural attraction located on the island of Sabtang in the province of Batanes, Philippines. 
+                    It is renowned for its picturesque landscape, characterized by towering cliffs, lush greenery, and pristine white sand beaches.
+                    </p>
+                </div>
+            </div>
+            <div class="item" id="list">
+                <img src="img/9.png">
+                <div class="content">
+                    <p>Surigao, Del Sur</p>
+                    <h2>Hinatuan River</h2>
+                    <p>
+                        The Hinatuan Enchanted River is a captivating natural wonder located in Barangay Talisay, Hinatuan, Surigao del Sur,
+                         Philippines. This majestic river is renowned for its stunning blue waters, which are crystal clear and seemingly enchanted, 
+                         giving rise to its name.
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/10.png">
+                <div class="content">
+                    <p>Sagada Mt. Province</p>
+                    <h2>Hanging Coffins </h2>
+                    <p>
+                    Hanging coffins, practiced by indigenous tribes, involve suspending coffins from cliffs or caves, symbolizing a connection to the heavens.
+                    Meanwhile, Sumaguing Cave in Sagada, Philippines, offers adventurous explorers a journey through stunning rock formations, underground rivers,
+                    and natural pools, showcasing the region's natural beauty.
+                    </p>
+                </div>
+            </div>
+            
+            
+        </div>
+        <!-- button arrows -->
+        <div class="arrows">
+            <button id="prev"><</button>
+            <button id="next">></button>
+        </div>
+        <!-- thumbnail -->
+        <div class="thumbnail">
+            <div class="item active">   
+                <img src="img/mayon1.jpeg">
+                <div class="content">
+                Mayon Volcano
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/island.jpeg">
+                <div class="content">
+                Hundred Island
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/les cabanes.jpeg">
+                <div class="content">
+                Las Cabanas Beach
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/little borac.jpg">
+                <div class="content">
+                Little Boracay
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/lihhthouse.jpeg">
+                <div class="content">
+                Basco Lighthouse
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/Calle Crisologo.jpeg">
+                <div class="content">
+                Calle Crisologo
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/Fort Santiago.jpeg">
+                <div class="content">
+                Fort Santiago
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/Chamantad Tinan.jpg">
+                <div class="content">
+                Chamantad Tinan
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/Hinatuan Enchanted RIver.jpg">
+                <div class="content">
+                Hinatuan River
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/Hanging Coffins and Sumaging Cave.jpg">
+                <div class="content">
+                Hanging Coffins
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="about" id="about">
+        <div class="left">
+            <img src="img/pexels-photo-flasssssssssssss.webp">
+        </div>
+        <div class="right">
+            <h5>Discover the Philippines: Where Dreams Come Alive</h5>
+            <p>Get ready for a journey like no other, where every moment is a melody and every sight a symphony. From the emerald isles to the bustling cities, the Philippines invites you to dive into a world of endless wonder and unforgettable experiences. Let your senses awaken as you explore paradise found in every corner of this breathtaking archipelago.</p>
+        </div>
+    </div>
+
+    <div class="attraction" id="att">
+        <div class="header">
+            <div class="info">
+                <h5>View More</h5>
+            </div>
+        </div>
+
+        <div class="attract-items">
+            <div class="item">
+                <img src="img/pic1.jpeg">
+                <div class="info">
+                    <h4>What makes the Philippines a must-see destination?</h4>
+                    <p>
+                    With its breathtaking beaches, diverse culture, and thrilling adventures like volcano trekking and island hopping, the Philippines offers unforgettable experiences for travelers seeking both natural wonders and cultural immersion.
+                    </p>
+                    <a href="https://www.youtube.com/watch?v=ZjFzkhrqIZs">View in Youtube <i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/free-photo-of-birds-eye-view-of-a-man-surfing-on-the-sea-water.jpeg">
+                <div class="info">
+                    <h4>How does the Philippines offer unique and memorable experiences for travelers?</h4>
+                    <p>       
+                        The Philippines offers a wide range of activities for adventurous souls. From trekking to the crater of an active volcano in Mt. Pinatubo to surfing the waves in Siargao, or activities like canyoneering in Kawasan Falls.
+                    </p>
+                    <a href="https://www.youtube.com/watch?v=ThtXz7RILWs">View in Youtube <i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/pic2.jpeg">
+                <div class="info">
+                    <h4>What sets the Philippines apart as a travel destination?</h4>
+                    <p>
+                    With its blend of stunning landscapes, vibrant festivals, and endless opportunities for adventure, the Philippines beckons explorers to discover its enchanting beauty and warm hospitality.
+                    </p>
+                    <a href="https://www.youtube.com/shorts/yjvOK6Qnkqo">View in Youtube <i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+
+    <footer>
+    <h5>Philippines</h5>
+        <div class="top">
+            <div class="social-links">
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-instagram'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+                <a href="#"><i class='bx bxl-linkedin-square'></i></a>
+            </div>
+        </div>
+        <div class="separator"></div>
+        <div class="bottom">
+            <div class="links">
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms of Service</a>
+                <a href="#">Cookies Setting</a>
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="index.js"></script>
+</body>
+</html>
